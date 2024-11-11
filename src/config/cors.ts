07d873e4b8cbaa0f128aedbe4d5e0a1a225be0b4
JamesTalamo <148525820+JamesTalamo@users.noreply.github.com>
2024-11-11 23:29:42 +0800

    added deployment url and build preview url

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,7 @@ config();
 
 const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
   ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:5173", "https://animeweeb-dev.onrender.com"];
+  : ["http://localhost:5173", "https://animeweeb-dev.onrender.com","http://localhost:4173"];
 
 const corsConfig = cors({
   allowMethods: ["GET"],
