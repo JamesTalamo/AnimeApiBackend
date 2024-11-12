0177dc1aad3e3ddc0001547df7fb01b44f7ad5eb
James Talamo <jamestalamo4@gmail.com>
2024-11-12 21:47:11 +0800

    changed

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,14 +5,15 @@ config();
 
 const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
   ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["https://animeweeb-dev.onrender.com", "http://localhost:4173"];
+  // : ["https://animeweeb-dev.onrender.com", "http://localhost:4173"];
+  : ["https://animeweeb-dev.onrender.com"];
 
 
 const corsConfig = cors({
   allowMethods: ["GET"],
   maxAge: 600,
   credentials: true,
-  origin: (origin) => (allowedOrigins.includes(origin) ? origin : null),
+  origin: allowedOrigins,
 });
 
 export default corsConfig;
