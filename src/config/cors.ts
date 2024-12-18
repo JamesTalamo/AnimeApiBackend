371dbc2ed9d726571a7b1210e4c36a4946b7f64a
JamesDev <148525820+JamesTalamo@users.noreply.github.com>
2024-12-19 01:29:39 +0800

    Update3 cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -6,14 +6,14 @@ config();
 // const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
 //   ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
 //   : ["https://animeweeb.vercel.app", "http://localhost:4173","http://localhost:5173", "*"];
-const allowedOrigins = ["*"];  // This will allow any origin
 
 
 const corsConfig = cors({
   allowMethods: ["GET"],
   maxAge: 600,
   credentials: true,
-  origin: allowedOrigins,
+  // origin: allowedOrigins,
+  origin: "*",
 });
 
 export default corsConfig;
